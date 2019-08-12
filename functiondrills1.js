e9cafda9cb81d4b9a8d20e7ddcce6636c5e0d732
github username <github email address>
2019-08-12 16:55:24 -0500

    fd1 complete

--- functiondrills1.js
+++ functiondrills1.js
@@ -1,10 +1,28 @@
-function yearOfBirth(age){
-return 2019-age;
+function getYearOfBirth(age){
+    if (age < 0){
+        throw new Error ("Age can not be negative");
+    }
+    return 2019 - age;
 }
 
 function createGreeting(name, age){
-return `Hi, my is ${name} and I'm ${age} years old"`;
+    if (name === undefined || age === undefined){
+        throw new Error ('Arguments not valid')}
+
+
+    if (typeof age !== 'number'){
+        throw new Error('Age must be a number');}
+        
+        
+    const yearOfBirth = getYearOfBirth(age);  
+    return `Hi, my is ${name} and I'm ${age} years old and I was born in ${yearOfBirth}`;
 }
 
-const greeting1 = createGreeting();
-console.log(greeting1);
+try {
+    const greeting1 = createGreeting('Preet', 31);
+    console.log(greeting1);
+}
+
+catch (error){
+    console.error(error);
+}
