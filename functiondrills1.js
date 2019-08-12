7bc73405e8eb247f19206f111e354d43dccfa779
github username <github email address>
2019-08-12 15:50:43 -0500

    update to function

--- functiondrills1.js
+++ functiondrills1.js
@@ -1,1 +1,6 @@
-console.log("Hi, my name is Preet and I'm 31 years old")
+function createGreeting(name, age){
+return `Hi, my is ${name} and I'm ${age} years old"`;
+}
+
+const greeting1 = createGreeting();
+console.log(greeting1);
